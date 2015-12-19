349fd8e9964d74bf386f3ffd164287cde7f2536d
Brandon Rukin <rukin.brandon@gmail.com>
2015-12-19 18:20:36 -0500

    #8 - A New Modular Component

--- src/Index.js
+++ src/Index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route } from 'react-router';
 import App from './components/App';
-import PoweredBy from './components/Powered-by';
 import About from './components/About';
 
 window.React = React;
@@ -11,7 +10,6 @@ render(
   (<Router>
     <Route path="/" component={App}>
       <Route path="/about" component={About}/>
-      <Route path="/poweredby" component={PoweredBy}/>
     </Route>
   </Router>), document.getElementById('content')
 );
---
+++ src/components/ContactList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default React.createClass({
+	render() {
+		return (
+			<section>
+				<h2>Contact List</h2>
+			</section>
+		)
+	}
+});
